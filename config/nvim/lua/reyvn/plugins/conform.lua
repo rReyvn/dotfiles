f9dcb76e527691436191152d5e3a6c8b5d3ead10
rReyvn <reyvn029@gmail.com>
2024-05-14 22:56:44 +0700

    Neovim: Add Linter

--- config/nvim/lua/reyvn/plugins/conform.lua
+++ config/nvim/lua/reyvn/plugins/conform.lua
@@ -1,38 +1,36 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>bf",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				desc = "Format buffer",
-			},
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>bf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			desc = "Format buffer",
 		},
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				css = { { "prettierd", "prettier" } },
-				html = { { "prettierd", "prettier" } },
-				-- python = { "isort", "black" },
-				-- blade = { "blade_formatter" },
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500, async = true, lsp_fallback = true },
-			-- Customize formatters
-			formatters = {
-				shfmt = {
-					prepend_args = { "-i", "2" },
-				},
-			},
-		},
-		init = function()
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
 	},
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			-- python = { "isort", "black" },
+			-- blade = { "blade_formatter" },
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
+	},
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
