391d6936bc7a9393008245f05b12a74bc1def1da
rReyvn <reyvn029@gmail.com>
2024-06-06 01:25:39 +0700

    WM: Doing some script things

--- term/nvim/lua/reyvn/opts/runner/five-server.lua
+++
@@ -1,3 +0,0 @@
-require("fs").setup({
-	notify = true,
-})
--- term/nvim/lua/reyvn/plugins/runner.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"Diogo-ss/five-server.nvim",
-	cmd = { "FiveServer" },
-	build = function()
-		require("fs.utils.install")()
-	end,
-}
